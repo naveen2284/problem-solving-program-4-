1de290033cbc56df73e5a036f0cd5ad389f877e3
naveen2284 <93900677+naveen2284@users.noreply.github.com>
2021-11-25 02:05:39 -0800

    Create temparature.c

---
+++ temparature.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()tempa
+{
+    float c,f;
+    printf("to find out temperature");
+    scanf("%f",&f);
+    c=0.5*(f-32);
+    printf("%f",c);
+    
+
+    return 0;
+}
